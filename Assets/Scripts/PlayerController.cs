0f03035b4d18429b4bdc07a9d8f11e2ad35f5c4d
Nickolay Klyukay <klyukay@gmail.com>
2017-09-28 21:33:52 +0300

    Ready

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	Rigidbody rb;
+	int count = 0;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody> ();
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical).normalized * speed * Time.fixedDeltaTime;
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = string.Format ("Count: {0}", count);
+		if (count >= 10) winText.text = "You Win!";
+			
+	}
+
+}
